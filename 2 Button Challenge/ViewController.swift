423c2889e8801c539a7a4c4939a3c126e5c0fb7f
Alex <amcclain1985@gmail.com>
2021-09-03 17:07:05 -0400

    Initial Commit

---
+++ 2 Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2 Button Challenge
+//
+//  Created by Alexandre McClain on 9/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
